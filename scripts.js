75243693991c469fb427cda69a772037451ecefc
NikolasRR <nkls99@gmail.com>
2022-02-14 17:00:59 -0300

    se eu tirar acima das expectativas eu pago uma cerveja pra Galdino

--- scripts.js
+++ scripts.js
@@ -1,36 +1,74 @@
-let userName = prompt("Digite seu nome");
-let userNameObject = {name: userName}
+// INITIALIZATION
+let userName = null;
+let userNameObject = {};
 
-let userNameInputPromisse = axios.post("https://mock-api.driven.com.br/api/v4/uol/participants", userNameObject);
-userNameInputPromisse.then(userNameAvailable);
-userNameInputPromisse.catch(userNameInUse);
+let userNameInputPromisse = null;
 
-function userNameInUse (serverError) {
-    userName = prompt("Nome já está em uso, digite outro.");
-    userNameObject.name = userName;
-    userNameInputPromisse = axios.post("https://mock-api.driven.com.br/api/v4/uol/participants", userNameObject);
-    userNameInputPromisse.then(userNameAvailable);
-    userNameInputPromisse.catch(userNameInUse);
+function login (event) {
+    if (event.key === "Enter" || event.key === undefined) {
+        userName = document.querySelector(".loginScreen input").value;
+        userNameObject = {name: userName};
+
+        userNameInputPromisse = axios.post("https://mock-api.driven.com.br/api/v4/uol/participants", userNameObject);
+
+        const userNameInput = document.querySelector(".loginScreen input");
+        userNameInput.classList.add("hidden");
+        const loginButton = document.querySelector(".loginScreen button");
+        loginButton.classList.add("hidden");
+        const loadingGif = document.querySelector(".loadingGif");
+        loadingGif.classList.remove("hidden");
+        const loadingText = document.querySelector(".loginScreen p");
+        loadingText.classList.remove("hidden");
+        const userNameNotAvailableWarning = document.querySelector(".loginScreen span");
+        userNameNotAvailableWarning.classList.add("hidden");
+
+        userNameInputPromisse.then(userNameAvailable);
+        userNameInputPromisse.catch(userNameNotAvailable);
+    }
+}
+
+function userNameNotAvailable () {
+    const userNameInput = document.querySelector(".loginScreen input");
+    userNameInput.classList.remove("hidden");
+
+    const loginButton = document.querySelector(".loginScreen button");
+    loginButton.classList.remove("hidden");
+
+    const loadingGif = document.querySelector(".loadingGif");
+    loadingGif.classList.add("hidden");
+
+    const loadingText = document.querySelector(".loginScreen p");
+    loadingText.classList.add("hidden");
+
+    const userNameNotAvailableWarning = document.querySelector(".loginScreen span");
+    userNameNotAvailableWarning.classList.remove("hidden");
+
+    document.querySelector(".loginScreen input").value = "";
 }
 
 function statusUpdater () {
-    axios.post("https://mock-api.driven.com.br/api/v4/uol/status", userNameObject);
+    const stayOnline = axios.post("https://mock-api.driven.com.br/api/v4/uol/status", userNameObject);
+    stayOnline.catch(couldntSendMessage);
 }
 
 function userNameAvailable (serverAnswer) {
+    const loginScreen = document.querySelector(".loginScreen");
+    loginScreen.classList.add("hidden");
+
     const threeSeconds = 3000;
     const fiveSeconds = 5000;
-    setInterval(getMessages, threeSeconds); 
-    setInterval(statusUpdater. fiveSeconds);
+    setInterval(getServerMessages, threeSeconds); 
+    setInterval(statusUpdater, fiveSeconds);
 }
 
-function getMessages () {
+function getServerMessages () {
     let getMessagesPromisse = axios.get("https://mock-api.driven.com.br/api/v4/uol/messages");
-    getMessagesPromisse.then(lookForNewMessages);
+    getMessagesPromisse.then(serverAnswerProcessor);
 }
 
-function lookForNewMessages (serverAnswer) {
+function serverAnswerProcessor (serverAnswer) {
     const messagesObject = serverAnswer.data;
+    document.querySelector(".messages").innerHTML = "";
     messagesObject.forEach(messageRenderer);
 }
 
@@ -38,38 +76,132 @@ function messageRenderer (message) {
     const messagesSection = document.querySelector(".messages");
     if (message.type === "status") {
         messagesSection.innerHTML = messagesSection.innerHTML + 
-        `<article class="statusMessage"><span>(${message.time})</span>
-        <p><b>${message.from}</b> ${message.text}</p></article>`
+        `<article class="statusMessage">
+            <span>(${message.time})</span>
+            <p><b>${message.from}</b> ${message.text}</p>
+         </article>`;
     } 
     else if (message.type === "message") {
         messagesSection.innerHTML = messagesSection.innerHTML + 
-        `<article class="message"><span>(${message.time})</span>
-        <p><b>${message.from}</b> para <b>Todos</b> ${message.text}</p></article>`
+        `<article class="message">
+            <span>(${message.time})</span>
+            <p><b>${message.from}</b> para <b>${message.to}</b> ${message.text}</p>
+         </article>`;
     }
-    else {
+    else if (message.to === userName || message.from === userName) {
         messagesSection.innerHTML = messagesSection.innerHTML + 
-        `<article class="privateMessage"><span>(${message.time})</span>
-        <p><b>${message.from}</b> para <b>${message.to}</b> ${message.text}</p></article>`
+        `<article class="privateMessage">
+            <span>(${message.time})</span>
+            <p><b>${message.from}</b> reservadamente para <b>${message.to}</b> ${message.text}</p>
+         </article>`;
     }
+
     const lastMessage = document.querySelector(".messages article:last-child");
-    lastMessage.scrollIntoView();
+    if (lastMessage !== null) {
+        lastMessage.scrollIntoView();
+    }
 }
 
-function sendMessage () {
-    let message = document.querySelector("input").value;
-    const messagesObject = {
-        from: userName,
-        to: "Todos",
-        text: message,
-        type: "message"
+// MESSAGE SENDING
+function sendMessageWithEnter (event) {
+    if (event.key === "Enter" || event.key === undefined) {
+
+        let message = document.querySelector("footer input").value;
+        const visibilityIsPublic = document.querySelector(".visibilitySelection .selected span").textContent === "Público";
+        let visibilityChosen = null;
+        console.log(visibilityIsPublic)
+
+        if (visibilityIsPublic) {
+            visibilityChosen = "message";
+        }
+        else {
+            visibilityChosen = "private_message";
+        }
+
+        const messagesObject = {
+            from: userName,
+            to: selectedReceiverSaved,
+            text: message,
+            type: visibilityChosen
+        };
+        document.querySelector("footer input").value = "";
+        const sendMessagePromisse = axios.post("https://mock-api.driven.com.br/api/v4/uol/messages", messagesObject);
+        sendMessagePromisse.then(getServerMessages);
+        sendMessagePromisse.catch(couldntSendMessage);
+
     }
-    document.querySelector("input").value = "";
-    const sendMessagePromisse = axios.post("https://mock-api.driven.com.br/api/v4/uol/messages", messagesObject);
-    sendMessagePromisse.then(getMessages)
-    sendMessagePromisse.catch(couldntSendMessage)
 }
 
 function couldntSendMessage (resposta) {
     alert("Você foi desconectado, a página será atualizada");
     window.location.reload();
 }
+
+// MESSAGE OPTIONS AND ONLINE USERS LIST
+function openAndCloseMessageReceiverOptions () {
+    const receiverOptions = document.querySelector("aside");
+    receiverOptions.classList.toggle("hidden");
+    const tenSeconds = 10000;
+    setInterval(getUsers, 10000);
+    getUsers();
+}
+
+function getUsers () {
+    const onlineUsersPromisse = axios.get("https://mock-api.driven.com.br/api/v4/uol/participants");
+    onlineUsersPromisse.then(loadUsers);
+}
+
+function loadUsers (serverAnswer) {
+    const onlineUsers = serverAnswer.data;
+    document.querySelector(".receiverSelection").innerHTML = `<div onclick="selectThisReceiver(this);">
+                                                                <ion-icon class="ionicon" name="people"></ion-icon>
+                                                                <span>Todos</span>
+                                                                <ion-icon class="check" name="checkmark-sharp"></ion-icon>
+                                                              </div>`;
+    onlineUsers.forEach(renderUsers);
+}
+
+function renderUsers (participant) {
+    const onlineUsers = document.querySelector(".receiverSelection");
+    if (participant.name == selectedReceiverSaved) {
+        onlineUsers.innerHTML += `<div calss="selected" onclick="selectThisReceiver(this);">
+                                <ion-icon class="ionicon" name="person-circle"></ion-icon>
+                                <span>${participant.name}</span>
+                                <ion-icon class="check hidden" name="checkmark-sharp"></ion-icon>
+                              </div>`;
+        return
+    }
+    onlineUsers.innerHTML += `<div onclick="selectThisReceiver(this);">
+                                <ion-icon class="ionicon" name="person-circle"></ion-icon>
+                                <span>${participant.name}</span>
+                                <ion-icon class="check hidden" name="checkmark-sharp"></ion-icon>
+                              </div>`;
+}
+
+let selectedReceiverSaved = null;
+
+function selectThisReceiver (newReceiver) {
+    const publicVisibility = newReceiver.classList.contains("public");
+    const privateVisibility = newReceiver.classList.contains("private");
+    const previousSelected = document.querySelector(".receiverSelection .selected");
+    if (publicVisibility || privateVisibility) {
+        const selectedReceiver = document.querySelector(".visibilitySelection .selected");
+        selectedReceiver.classList.remove("selected");
+        newReceiver.classList.add("selected");
+        
+    } else if (previousSelected !== null) {
+        previousSelected.classList.remove("selected");
+        newReceiver.classList.add("selected");
+        selectedReceiverSaved = newReceiver.innerText;
+        console.log(selectedReceiverSaved);
+    } else {
+        newReceiver.classList.add("selected");
+        selectedReceiverSaved = newReceiver.innerText;
+        console.log(selectedReceiverSaved);
+    }
+
+    const visibility = document.querySelector(".visibilitySelection .selected").innerText.toLowerCase();
+    const receiverInfoDisplay = document.querySelector("footer span");
+    receiverInfoDisplay.innerHTML = `Enviando para ${selectedReceiverSaved} (${visibility})`
+}
+
